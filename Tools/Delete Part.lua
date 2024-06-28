15a0981d25794b93402003e33a3c6dcc47ab4f8f
mattscy <mattscy@outlook.com>
2024-06-28 21:11:28 +0800

    fasfdasf

---
+++ Tools/Delete Part.lua
@@ -0,0 +1,59 @@
+local PLR = game:GetService("Players")
+
+local deleteHighlight
+local selectedPart
+
+local placementMaid = {}
+
+
+function GetLocalPlayer()
+    local player = PLR:GetPlayerFromCharacter(script.Parent.Parent)
+    return elemento:GetLocalPlayer(player)
+end
+
+
+function StartDeleting()
+    StopDeleting()
+
+    local lp = GetLocalPlayer()
+    local mouse = lp:GetMouse()
+
+    deleteHighlight = Instance.new("Highlight")
+    deleteHighlight.FillTransparency = 0.5
+    deleteHighlight.FillColor = Color3.new(1, 0, 0)
+    deleteHighlight.Archivable = false
+    deleteHighlight.Parent = lp:GetLocalFolder()
+
+    table.insert(placementMaid, mouse.Move:Connect(function()
+        selectedPart = mouse.Target
+        deleteHighlight.Adornee = selectedPart
+    end))
+end
+
+
+function StopDeleting()
+    for _, conn in pairs(placementMaid) do
+        conn:Disconnect()
+    end
+    placementMaid = {}
+
+    if deleteHighlight then
+        deleteHighlight:Destroy()
+        deleteHighlight = nil
+    end
+end
+
+
+function Delete()
+    if selectedPart then
+        selectedPart:Destroy()
+        selectedPart = nil
+
+        StartDeleting()
+    end
+end
+
+
+script.Parent.Equipped:Connect(StartDeleting)
+script.Parent.Unequipped:Connect(StopDeleting)
+script.Parent.Activated:Connect(Delete)
